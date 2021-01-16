ad7708ed8876ab09f283a953280491163a001c5b
Ronda Cowen <cowenhome5@gmail.com>
2021-01-15 23:59:16 -0600

    commit tree 7eac1e55c676d7903f9ca07f28f81d55459d1e21

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
 let addToy = false;
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+  fetch("http://localhost:3000/toys")
+   .then(r => r.json())
+   .then(toys => {
+    toysHtml = toys.map(function(toy){
+      return `
+      <div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button class="like-btn">Like <3</button>
+    </div>
+      `
+    })
+    document.querySelector("#toy-collection").innnerHtml = 
+    toysHtml.join('')
+
+   })
+   toyForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    const toyName = e.target.name.value
+    const toyImage = e.target.image.value
+    fetch("http://localhost:3000/toys", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: toyName,
+        image: toyImage,
+        likes: "99 likes"
+      })
+    })
+    .then( r => r.json())
+    .then( newToy => {
+      
+    })
+   })
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
